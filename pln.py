f66f83fa9d21ef2de7c9d981de1e5177f609a5cb
arthurhbu <arthurhbu@gmail.com>
2025-07-14 22:07:10 -0300

    Criação função para preprocessamento

--- pln.py
+++ pln.py
@@ -1,6 +1,7 @@
 import fitz
-# import spacy
-
+import spacy
+import nltk
+nlp = spacy.load('en_core_web_sm')
 path = './pdf'
 
 def extract_pdf_text(pdf_path):
@@ -14,9 +15,11 @@ def separate_references(text):
     final_half = text[len(text)//2:]
     position_reference = -1
     
+    contador = 0
     references_keywords = ['REFERENCES', 'REFERENCES AND NOTES', 'REFERENCES AND ACKNOWLEDGEMENTS', 'REFERENCES AND ACKNOWLEDGMENTS', 'References', 'References and Notes', 'References and Acknowledgements', 'References and Acknowledgments', 'Bibliography', 'BIBLIOGRAPHY']
     
     for keyword in references_keywords:
+        contador += 1
         position = final_half.find(keyword)
         if position != -1:
             position_reference = position + len(text)//2
@@ -25,15 +28,31 @@ def separate_references(text):
     if position_reference != -1:
         text_body = text[:position_reference]
         text_references = text[position_reference:]
+        print(contador)
         return text_body, text_references
+    
     else:
         return text, ""
 
+def preprocessing_text(text):
+    doc = nlp(text.lower())
+    clean_tokens = []
+
+    for token in doc: 
+        if not token.is_stop and not token.is_punct and not token.is_space and token.is_alpha:
+            clean_tokens.append(token.text)
+
+    return clean_tokens
+
+def extract_keywords(clean_tokens):
+    ...
 
 
 text = extract_pdf_text(f'{path}/A_High_Performance_Blockchain_Platform_for_Intelligent_Devices.pdf')
+# print(text)
 text_body, text_references = separate_references(text)
-print(text_references)
+preprocessed_text_body = preprocessing_text(text_body)
+print(preprocessed_text_body)
 
 
 
